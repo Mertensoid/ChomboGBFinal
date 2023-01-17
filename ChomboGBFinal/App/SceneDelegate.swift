45e7bbe46cf6a19f5b44c9f4b5bac9100ec0679f
Mertensoid <matsoukm@yandex.ru>
2023-01-17 14:26:43 +0300

    project structure

---
+++ ChomboGBFinal/App/SceneDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  SceneDelegate.swift
+//  ChomboGBFinal
+//
+//  Created by admin on 17.01.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions) {
+            guard let windowScene = (scene as? UIWindowScene) else {
+                return
+            }
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = ViewController()
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+}
+
