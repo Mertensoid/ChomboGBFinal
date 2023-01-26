a6b21b854873ff4c3c1623af7b328aff48335bb2
Mertensoid <matsoukm@yandex.ru>
2023-01-26 14:12:38 +0300

    #25 MVP file structure

---
+++ ChomboGBFinal/App/Flows/Main/Profile/Settings/Presenter/SettingsPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsPresenter.swift
+//  ChomboGBFinal
+//
+//  Created by admin on 26.01.2023.
+//
+
+import Foundation
---
+++ ChomboGBFinal/App/Flows/Main/Tools/ToolDetails/Builder/ToolDetailsBuilder.swift
@@ -0,0 +1,8 @@
+//
+//  ToolDetailsBuilder.swift
+//  ChomboGBFinal
+//
+//  Created by admin on 26.01.2023.
+//
+
+import Foundation
