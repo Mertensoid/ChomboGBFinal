aed16fb67624dcb563fe241f2ee6fbbd31aae84b
Mertensoid <matsoukm@yandex.ru>
2023-01-31 11:36:49 +0300

    #34 UIView extension

---
+++ ChomboGBFinal/Core/Extensions/UIView + ext.swift
@@ -0,0 +1,14 @@
+//
+//  UIView + ext.swift
+//  ChomboGBFinal
+//
+//  Created by admin on 31.01.2023.
+//
+
+import UIKit
+
+extension UIView {
+    func activateConstraints() {
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
