19b4551218c3c5d7a8c4919eed5e2dbe200b669c
Mertensoid <matsoukm@yandex.ru>
2023-02-03 15:02:51 +0300

    #29 output delegate

---
+++ ChomboGBFinal/App/Flows/Login/Presenter/LoginViewOutputDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewOutputDelegate.swift
+//  ChomboGBFinal
+//
+//  Created by admin on 03.02.2023.
+//
+
+import Foundation
+
+protocol LoginViewOutputDelegate {
+    func checkAccountData()
+    func showRegistrationScreen()
+}
