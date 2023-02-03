95086deed9d95518d0f0e7032ae481069e42b390
Mertensoid <matsoukm@yandex.ru>
2023-02-03 15:05:12 +0300

    #29 presenter + view controller refactoring

--- ChomboGBFinal/App/Flows/Login/Presenter/LoginPresenter.swift
+++ ChomboGBFinal/App/Flows/Login/Presenter/LoginPresenter.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+class LoginPresenter {
+    
+    // MARK: - Private properties
+    
+    weak private var view: LoginViewController?
+    
+    // MARK: - Construction
+    
+    required init(view: LoginViewController) {
+        self.view = view
+    }
+}
+
+extension LoginPresenter: LoginViewOutputDelegate {
+    
+    func checkAccountData() {
+        print("Account checking")
+        view?.clearLoginTextField()
+        view?.clearPasswordTextField()
+        let mainTabBar = MainTabBarController()
+        mainTabBar.modalPresentationStyle = .fullScreen
+        view?.present(mainTabBar, animated: true)
+    }
+    
+    func showRegistrationScreen() {
+        view?.clearLoginTextField()
+        view?.clearPasswordTextField()
+        let registrationVC = RegistrationViewController()
+        registrationVC.modalPresentationStyle = .fullScreen
+        view?.present(registrationVC, animated: true)
+    }
+}
