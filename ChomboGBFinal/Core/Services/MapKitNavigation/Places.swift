4e656d4eb57f3438a419a6cb693570996afbca28
Stanislav96RUS <107839596+Stanislav96RUS@users.noreply.github.com>
2023-02-06 12:22:17 +0500

    feature/#44-MapNavigation-AddAnnotation
    - add map display
    - center on current location
    - mark points with warehouse points (coordinates are set locally manually in a separate json file)
    - when you click on a point, open an annotation with a description and a special button, when clicked, it transfers to AppleMap and offers to build a route to a given point

--- ChomboGBFinal/Core/Services/MapKitNavigation/Places.swift
+++ ChomboGBFinal/Core/Services/MapKitNavigation/Places.swift
@@ -6,3 +6,82 @@
 //
 
 import Foundation
+import MapKit
+import Contacts
+
+class Places: NSObject, MKAnnotation {
+    
+    let title: String?
+    let locationName: String?
+    let discipline: String?
+    let coordinate: CLLocationCoordinate2D
+    
+    
+    init (
+        title: String?,
+        locationName: String?,
+        discipline: String?,
+        coordinate: CLLocationCoordinate2D
+        )
+    {
+        self.title = title
+        self.locationName = locationName
+        self.discipline = discipline
+        self.coordinate = coordinate
+        
+        
+        super.init()
+    }
+    
+    init?(feature: MKGeoJSONFeature) {
+        guard
+            let point = feature.geometry.first as? MKPointAnnotation,
+            let propertiesData = feature.properties,
+            let json = try? JSONSerialization.jsonObject(with: propertiesData),
+            let properties = json as? [String: Any]
+        else {
+            return nil
+        }
+        
+        title = properties["title"] as? String
+        locationName = properties["location"] as? String
+        discipline = properties["discipline"] as? String
+        coordinate = point.coordinate
+        super.init()
+    }
+    
+    var subtitle: String? {
+        return locationName
+    }
+    
+    var mapItem: MKMapItem? {
+        
+        guard let location = locationName else {
+            return nil
+        }
+        
+        let addressDict = [CNPostalAddressStreetKey: location]
+        let placemark = MKPlacemark(coordinate: coordinate, addressDictionary: addressDict)
+        
+        let mapItem = MKMapItem(placemark: placemark)
+        mapItem.name = title
+        return mapItem
+    }
+    
+    
+    var image: UIImage {
+        guard let name = discipline else {
+            return #imageLiteral(resourceName: "123")
+        }
+        switch name {
+        case "Sklad":
+            return #imageLiteral(resourceName: "123")
+//        case "I":
+//            return #imageLiteral(resourceName: "123")
+        default:
+            return #imageLiteral(resourceName: "123")
+
+        }
+    }
+
+}
