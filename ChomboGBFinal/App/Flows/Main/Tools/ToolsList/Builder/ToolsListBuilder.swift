54b9aa6e54b6a400f2ba94abc5de8fb4fabe1670
Mertensoid <matsoukm@yandex.ru>
2023-02-09 15:18:33 +0300

    #28 tools list builder

--- ChomboGBFinal/App/Flows/Main/Tools/ToolsList/Builder/ToolsListBuilder.swift
+++ ChomboGBFinal/App/Flows/Main/Tools/ToolsList/Builder/ToolsListBuilder.swift
@@ -5,4 +5,13 @@
 //  Created by admin on 26.01.2023.
 //
 
-import Foundation
+import UIKit
+
+class ToolsListBuilder {
+    static func createToolsListScreen() -> UIViewController {
+        let view = ToolsListViewController()
+        let presenter = ToolsListPresenter(delegate: view)
+        view.presenter = presenter
+        return view
+    }
+}
