3593dfe4923e8d4e949902a85d7e6d39d4ae5991
Mertensoid <matsoukm@yandex.ru>
2023-02-09 15:09:07 +0300

    #28 tools list presenter + delegates

---
+++ ChomboGBFinal/App/Flows/Main/Tools/ToolsList/Presenter/ToolsListViewInputDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  ToolsListViewInputDelegate.swift
+//  ChomboGBFinal
+//
+//  Created by admin on 09.02.2023.
+//
+
+import Foundation
+
+protocol ToolsListViewInputDelegate: AnyObject {
+    func tableViewReloadData()
+}
